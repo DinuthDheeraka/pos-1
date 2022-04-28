8379a66cfb8ecd887a280fa0d03de3fcab47e248
DinuthDheeraka <dinuthdheeraka345@gmail.com>
2022-04-28 10:30:56 +0530

    Added POS System

---
+++ Layered_Architecture/src/dao/CustomerDAOImpl.java
@@ -0,0 +1,47 @@
+package dao;
+
+import db.DBConnection;
+import model.CustomerDTO;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+/**
+ * @author : Sanu Vithanage
+ * @since : 0.1.0
+ **/
+
+public class CustomerDAOImpl {
+
+    public ArrayList<CustomerDTO> getAllCustomers() throws SQLException, ClassNotFoundException {
+        Connection connection = DBConnection.getDbConnection().getConnection();
+        Statement stm = connection.createStatement();
+        ResultSet rst = stm.executeQuery("SELECT * FROM Customer");
+        ArrayList<CustomerDTO> allCustomers = new ArrayList<>();
+        while (rst.next()) {
+            String id = rst.getString(1);
+            String name = rst.getString(2);
+            String address = rst.getString(3);
+            allCustomers.add(new CustomerDTO(id, name, address));
+        }
+        return allCustomers;
+    }
+
+    public boolean insertCustomer(CustomerDTO customerDTO) throws SQLException, ClassNotFoundException {
+        Connection connection = DBConnection.getDbConnection().getConnection();
+        PreparedStatement pstm = connection.prepareStatement("INSERT INTO Customer (id,name, address) VALUES (?,?,?)");
+        pstm.setString(1,customerDTO.getId());
+        pstm.setString(2,customerDTO.getName());
+        pstm.setString(3,customerDTO.getAddress());
+        return pstm.executeUpdate()>0;
+    }
+
+    public boolean updateCustomer(CustomerDTO customerDTO) throws SQLException, ClassNotFoundException {
+        Connection connection = DBConnection.getDbConnection().getConnection();
+        PreparedStatement pstm = connection.prepareStatement("UPDATE Customer SET name=?, address=? WHERE id=?");
+        pstm.setString(1, customerDTO.getName());
+        pstm.setString(2, customerDTO.getAddress());
+        pstm.setString(3, customerDTO.getId());
+        return pstm.executeUpdate()>0;
+    }
+}
